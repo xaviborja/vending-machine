1c090792a8909dca8b37e7637ebd408935a7c87f
Xavi Borja <xavier.borja@latostadora.com>
2021-08-06 16:58:36 +0200

    Initial setup

---
+++ tests/VendingMachineTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class VendingMachineTest extends TestCase
+{
+    public function testItWorks(): void
+    {
+        self::assertTrue(true);
+    }
+}
